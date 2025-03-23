821c581349e6a6b89c7cabe0f20e091434224e03
YourUsername <your-email@example.com>
2025-03-23 17:11:42 +0200

    cleaning project

--- flutter app (laptop_price_prediction)/android/app/src/main/kotlin/com/example/laptop_price_prediction_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.laptop_price_prediction_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter app (laptop_price_prediction)/lib/utils/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  // Use environment variables or secure storage in a real app
-  static const String apiBaseUrl = "http://192.168.1.72:8000"; // Replace in production
-  static const String apiEndpoint = "/predict";
-  static const int apiTimeout = 10;
-}
